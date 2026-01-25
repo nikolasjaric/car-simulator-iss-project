a8e13ed93354c19803eac7ca6d2383eca18e1473
Nikolas Jarić <nikolas.jaric15@gmail.com>
2026-01-25 21:10:51 +0100

    Add cleaned up code for CarController

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,8 +1,4 @@
-﻿// prije insertanja manual modea a s poboljsanim RPMom
-
-
-
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -103,7 +99,7 @@ public class CarController : MonoBehaviour
     }
 
     
-    // POKUSAJ DODAVANJA PARTICLESA
+    // DODAVANJE PARTICLESA
     private void CheckParticles()
 {
     CheckWheelParticle(colliders.FRWheel, wheelParticles.FRWheel, wheelParticles.FRWheelTrail);
@@ -179,25 +175,6 @@ private void CheckWheelParticle(
         ApplyWheelPositions();
     }
 
-
-
-    /*
-    private void UpdateUI()
-    {
-        rpmNeedle.rotation = Quaternion.Euler(
-            0f,
-            0f,
-            Mathf.Lerp(minNeedleRotation, maxNeedleRotation, RPM / (redLine * 1.1f))
-        );
-
-        rpmText.text = $"{RPM:0,000} rpm";
-        gearText.text = gearState == GearState.Neutral ? "N" : (currentGear + 1).ToString();
-
-        speed = colliders.RRWheel.rpm * colliders.RRWheel.radius * 2f * Mathf.PI / 10f;
-        speedClamped = Mathf.Lerp(speedClamped, speed, Time.deltaTime);
-    }
-
-    */
 
     private void UpdateUI()
 {
@@ -301,58 +278,6 @@ private void CheckWheelParticle(
     }
 
 
-/*
-private float CalculateTorque()
-    {
-        float torque = 0f;
-
-        if (RPM < idleRPM + 200 && gasInput == 0 && currentGear == 0)
-            gearState = GearState.Neutral;
-
-        if (gearState == GearState.Running && clutch > 0f)
-        {
-            if (RPM > increaseGearRPM)
-            {
-                StartCoroutine(ChangeGear(1));
-            }
-            else if (RPM < decreaseGearRPM && 1 <= currentGear)
-            {
-                StartCoroutine(ChangeGear(-1));
-            }
-        }
-
-        if (isEngineRunning > 0)
-        {
-            if (clutch < 0.1f)
-            {
-                RPM = Mathf.Lerp(
-                    RPM,
-                    Mathf.Max(idleRPM, redLine * gasInput) + Random.Range(-50, 50),
-                    Time.deltaTime
-                );
-            }
-            else
-            {
-                wheelRPM = Mathf.Abs((colliders.RRWheel.rpm + colliders.RLWheel.rpm) * 0.5f)
-                           * gearRatios[currentGear]
-                           * differentialRatio;
-
-                RPM = Mathf.Lerp(RPM, Mathf.Max(idleRPM - 100, wheelRPM), Time.deltaTime * 3f);
-
-                torque =
-                    hpToRPMCurve.Evaluate(RPM / redLine) * motorPower / RPM
-                    * gearRatios[currentGear]
-                    * differentialRatio
-                    * 5252f
-                    * clutch;
-
-            }
-        }
-
-        return torque;
-    }
-
-    */
 
 
     private float CalculateTorque()
